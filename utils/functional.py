66a6584c02ba8657a8d4def92af6e002694f0821
Aidilele <zhou2429060789@hotmail.com>
2024-09-26 17:00:47 +0800

    fix bugs

--- utils/functional.py
+++ utils/functional.py
@@ -2,12 +2,17 @@ import torch
 
 
 def norm_kl_div(u1, lv1, u2, lv2):
+    N = u1.shape[0] // u2.shape[0]
+    u2 = u2.repeat(N, 1)
+    lv2 = lv2.repeat(N, 1)
     v1 = (2 * lv1).exp()
     v2 = (2 * lv2).exp()
     kl = lv2 - lv1 + (v1 + (u1 - u2) ** 2) / v2 * 0.5 - 0.5
-    return kl
+    return kl.view(N, -1, u1.shape[-1]).mean(dim=0)
 
 
 def euc_distance(x, y):
+    N = x.shape[0] // y.shape[0]
+    y = y.repeat(N, 1)
     distances = torch.sqrt(torch.sum((x - y) ** 2, dim=1))
-    return distances
+    return distances.view(N, -1, x.shape[-1]).mean(dim=0)
