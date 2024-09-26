7d791de8a0b0e90aca4a05c2831ab79436560ae2
Aidilele <zhou2429060789@hotmail.com>
2024-09-26 20:03:24 +0800

    fix bugs

--- dataset/dataset_class.py
+++ dataset/dataset_class.py
@@ -36,7 +36,7 @@ class TaskDataset(Dataset):
         self.normalization = 0
         self.obs_normalization = 0
         self.batch_size = 128
-        self.tuple_size = 5
+        self.tuple_size = 4
         self.dataset_size = 0
 
         self.load(file='../datasets/MT10_rn5_sc0.5.pkl')
@@ -82,34 +82,51 @@ class TaskDataset(Dataset):
         self.data['traj_data'] = traj_data
         self.data['traj_mask'] = torch.from_numpy(traj_mask).to(torch.float32)
         self.data['traj_task'] = torch.from_numpy(traj_task).to(torch.float32)
-
+        self.dataset_size = len(self.data['traj_data'])
+        traj_max = []
+        traj_min = []
+        traj_max_mask = []
+        traj_min_mask = []
+        traj_task = []
         for env_index in range(10):
             start = env_index * 250
             end = start + 250
-            same_env_index = torch.randint(low=start, high=end, size=(self.batch_size, self.tuple_size))
-            diff_env_index1 = torch.randint(low=0, high=start, size=(self.batch_size, self.tuple_size))
-            diff_env_index2 = torch.randint(low=end, high=self.dataset_size, size=(self.batch_size, self.tuple_size))
+            same_env_index = torch.randint(low=start, high=end, size=(500, self.tuple_size)).sort().values
+            if env_index == 0:
+                diff_env_index = torch.randint(low=end, high=self.dataset_size, size=(500, self.tuple_size))
+            elif env_index == 9:
+                diff_env_index = torch.randint(low=0, high=start, size=(500, self.tuple_size))
+            else:
+                diff_env_index1 = torch.randint(low=0, high=start, size=(500, self.tuple_size // 2))
+                diff_env_index2 = torch.randint(low=end, high=self.dataset_size, size=(500, self.tuple_size // 2))
+                diff_env_index = torch.cat((diff_env_index1, diff_env_index2), dim=-1)
 
+            traj_max_index = same_env_index[:, -1]
+            traj_min_index = torch.cat((diff_env_index, same_env_index[:, :-1]), dim=-1)
+            traj_max_ = self.data['traj_data'][traj_max_index]
+            traj_min_ = self.data['traj_data'][traj_min_index]
+            traj_max_mask_ = self.data['traj_mask'][traj_max_index]
+            traj_min_mask_ = self.data['traj_mask'][traj_min_index]
+            traj_task_ = self.data['traj_task'][traj_max_index]
+            traj_max.append(traj_max_)
+            traj_min.append(traj_min_)
+            traj_max_mask.append(traj_max_mask_)
+            traj_min_mask.append(traj_min_mask_)
+            traj_task.append(traj_task_)
 
-        self.dataset_size = len(self.data['traj_aver'])
+        self.data['traj_max'] = torch.stack(traj_max, dim=0).view(500 * 10, 100, 82)
+        self.data['traj_min'] = torch.stack(traj_min, dim=0).view(500 * 10, -1, 100, 82)
+        self.data['traj_max_mask'] = torch.stack(traj_max_mask, dim=0).view(500 * 10, 100)
+        self.data['traj_min_mask'] = torch.stack(traj_min_mask, dim=0).view(500 * 10, -1, 100)
+        self.data['traj_task'] = torch.stack(traj_task, dim=0).view(500 * 10, 10)
 
     def sample(self):
-        sample_end = torch.randint(low=self.tuple_size, high=self.dataset_size, size=(self.batch_size,))
-        sample_index = []
-        for end in sample_end:
-            sample_index.append(torch.randint(low=0, high=end, size=(self.tuple_size,)))
-        sample_index = torch.stack(sample_index, dim=0)
-        traj_max = self.data['traj_data'][sample_end]
-        traj_min = self.data['traj_data'][sample_index]
-        shape = traj_min.shape
-        traj_min = traj_min.view(-1, shape[-2], shape[-1])
-
-        traj_max_mask = self.data['traj_mask'][sample_end]
-        traj_min_mask = self.data['traj_mask'][sample_index]
-        shape = traj_min_mask.shape
-        traj_min_mask = traj_min_mask.view(-1, shape[-1])
-
-        traj_task = self.data['traj_task'][sample_end]
+        sample_index = torch.randint(low=0, high=5000, size=(self.batch_size,))
+        traj_max = self.data['traj_max'][sample_index]
+        traj_min = self.data['traj_min'][sample_index]
+        traj_max_mask = self.data['traj_max_mask'][sample_index]
+        traj_min_mask = self.data['traj_min_mask'][sample_index]
+        traj_task = self.data['traj_task'][sample_index]
         return traj_max, traj_min, traj_max_mask, traj_min_mask, traj_task
 
 
